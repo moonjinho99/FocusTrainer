7cef3cca6b8d3f0cbc2624c28fba766c920fcb3d
moonjinho99 <anszl789@naver.com>
2025-11-05 14:40:30 +0900

    init : first commit

---
+++ src/main/java/com/mjh/focustrainer/FocustrainerApplication.java
@@ -0,0 +1,13 @@
+package com.mjh.focustrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocustrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocustrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjh/focustrainer/FocustrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjh.focustrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocustrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
